220d12667bdccba1d544c3b5a2db5cb6b454276e
Rama Krishna Pinnimty <ramakrishnapinnimty@gmail.com>
2022-08-24 00:29:20 -0400

    Time: 55 ms (48.08%), Space: 14.1 MB (15.83%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.py
@@ -0,0 +1,23 @@
+# Approach: 2-pointer; Time: O(n); Space: O(1)
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        # String leading & trailing ' ' ; then split on ' '
+        s = s.strip(' ').split(' ')
+
+        # Ignore the remnant extra ' ' by considering only alphanumeric characters
+        s = [word for word in s if word.isalnum()]
+
+        start = 0
+        end = len(s)-1
+        # Loop till `start` crosses `end`
+        while start < end:
+            # Swap
+            temp = s[start]
+            s[start] = s[end]
+            s[end] = temp
+
+            # Update pointers
+            start += 1
+            end -= 1
+
+        return ' '.join(s) # Re-join the words
